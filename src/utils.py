f8473675b45cf00901a7e740c6cf1bc08e7a718a
Sergey <sergey.tikin94@gmail.com>
2024-06-02 14:01:14 +0300

    add function formation_data

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+from dateutil import parser
 
 
 def load_operation():
@@ -16,3 +17,15 @@ def list_operations(operations):
     transfer_executed = [operation for operation in operations if operation.get("state") == "EXECUTED"]
     transfer_executed.sort(key=lambda x: str(x.get("date")), reverse=True)
     return transfer_executed[:5]
+
+
+def formation_data(date):
+    """
+    Форматирование даты
+    """
+    date = parser.parse(date).date()
+    operation = date.strftime("%d.%m.%Y")
+    return operation
+
+
+
