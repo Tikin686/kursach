33efbc178046e430c5713436f6bfb8d17ab2086e
Sergey <sergey.tikin94@gmail.com>
2024-06-02 14:37:54 +0300

    test formation card

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,9 @@
-from src.utils import formation_data
+from src.utils import formation_data, formation_card
 
 
 def test_formation_data():
     assert formation_data("2019-06-16T22:17:01.825020") == "16.06.2019"
+
+
+def test_formation_card():
+    assert formation_card("Maestro 1596837868705199") == "1596 83** **** 5199"
