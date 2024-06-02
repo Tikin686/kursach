421a3f1039c0e34e6c647041f01e038c90c7657f
Sergey <sergey.tikin94@gmail.com>
2024-06-02 13:16:25 +0300

    add function list_operation

--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,12 @@ def load_operation():
     """
     with open("operations.json", "r", encoding="UTF-8") as file:
         return json.load(file)
+
+
+def list_operations(operations):
+    """
+    Список операций
+    """
+    transfer_executed = [operation for operation in operations if operation.get("state") == "EXECUTED"]
+    transfer_executed.sort(key=lambda x: str(x.get("date")), reverse=True)
+    return transfer_executed[:5]
